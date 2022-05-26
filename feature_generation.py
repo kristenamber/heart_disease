c0d83888622c41ac1771d620053d88896429a2c6
kristenamber <79291623+kristenamber@users.noreply.github.com>
2022-05-25 18:12:06 -0600

    Add files via upload

---
+++ feature_generation.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+class FeatureGen
+    def __init__(self, df):
+        self.df = df
+
+    def ordinal_cat(self, feature_name):
+        pass
+
+
+    def one_hot(self, feature_name):
+        pass
+
+
+    def keep_feats(self, feature_list):
+        pass
+
+
+
+
+
+
+
---
+++ model.py
@@ -0,0 +1,21 @@
+
+
+class Model:
+    def __init__(self, model):
+        self.model = model
+
+
+    def train(self, X_train, y_train):
+        pass
+
+    def test(self, X_test, y_test):
+        pass
+
+
+    def get_confusion(self, data, labels):
+        pass
+
+
+    def get_mse(self, data, labels):
+        pass
+
